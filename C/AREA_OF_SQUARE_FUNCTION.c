6bec211cfd04e6962f327a65c0804df4bb661905
mayurikale2406 <mayurikale1404@gmail.com>
2023-09-02 19:22:27 +0530

    C/AREA_OF_SQUARE_FUNCTION.c

---
+++ C/AREA_OF_SQUARE_FUNCTION.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int square(int num)
+{
+	printf("\n enter a num");
+	scanf("%d",&num);
+	int result=num*num;
+}
+int main()
+{
+	int num;
+	int result=0;
+	printf("\n area of square %d",result);
+	scanf("%d",&result);
+}
