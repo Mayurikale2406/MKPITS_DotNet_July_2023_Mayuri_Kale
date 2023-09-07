4b0baac5f139f69111efb4f92f6439fc4960e1a7
mayurikale2406 <mayurikale1404@gmail.com>
2023-09-07 17:04:13 +0530

    CPP/

---
+++ CPP/add_sub_mul.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n1,n2;
+	int sum=0;
+	char op;
+	cout<<"\n enter 2 no : ";
+	cin>>n1>>n2;
+	cout<<"enter op(+,-,*,/)";
+	cin>>op;
+	if(op=='+')
+	{
+		sum=n1+n1;
+	}
+	else if(op=='-')
+	{
+		sum=n1-n2;
+	}
+	else if(op=='*')
+	{
+		sum=n1*n2;
+	}
+	else if(op=='/')
+	{
+		sum=n1/n2;
+	}
+	cout<<"sum ="<<sum;
+	return 0;
+	
+}
