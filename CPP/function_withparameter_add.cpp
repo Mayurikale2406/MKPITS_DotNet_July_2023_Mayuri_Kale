d1237a763a52bd458085fabe4aeef105f04bc96e
mayurikale2406 <mayurikale1404@gmail.com>
2023-09-17 13:40:15 +0530

    CPP/

---
+++ CPP/function_withparameter_add.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void addition(int n1,int n2)
+{
+	int result=n1+n2;
+	cout<<"addition of two num"<<result<<addition<<endl;
+}
+int main()
+{   
+    int num1,num2;
+    cout<<"enter a num";
+    cin>>num1>>num2;
+    addition(num1,num2);
+    
+    
+}
