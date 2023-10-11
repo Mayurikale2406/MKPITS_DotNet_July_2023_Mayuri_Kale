e7a89ef5a89ef06720398b445f1246a94b867599
mayurikale2406 <mayurikale1404@gmail.com>
2023-10-12 00:23:18 +0530

    csharp

---
+++ C#/add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace additionProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1,num2,add;
+
+            Console.WriteLine("Enter a num1");
+            num1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a num2");
+            num2=Convert.ToInt32(Console.ReadLine());
+            add=   num1+num2;
+            Console.WriteLine("Result : " + add);
+            Console.ReadKey();
+        }
+    }
+}
