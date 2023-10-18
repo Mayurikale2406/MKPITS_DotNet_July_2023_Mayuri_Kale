33c739ca3ee2f04ae01de847e693d9990e87e8c1
mayurikale2406 <mayurikale1404@gmail.com>
2023-10-18 18:41:53 +0530

    csharp

---
+++ C#/fabonacci_series_while.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = 0;
+            int num1 = 0, num2 = 1;
+            int add;
+
+            while (num < 10)
+            {
+                Console.WriteLine(num2);
+
+                add = num1 + num2;
+                num1 = num2;
+                num2 = add;
+
+                num++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
