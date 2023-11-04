6d7c5db3a6ff88216765426c267b9e1dbff1f9ab
mayurikale2406 <mayurikale1404@gmail.com>
2023-11-04 23:06:13 +0530

    csharp

---
+++ C#/constructor_product_form.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contructor_product_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            product b = new product(textBox1.Text,Convert.ToInt32( textBox2.Text),Convert.ToInt32( textBox3.Text));
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Product Name: " + b.ProductName + "\n");
+            sb.Append("Product Quantity: " + b.ProductQuantity + "\n");
+            sb.Append("Product Price : " + b.ProductPrice + "\n");
+            label4.Text = sb.ToString();
+
+        }
+    }
+}
---
+++ C#/copy_constructor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace program
+{
+    class Book
+    {
+        int bookid;
+        string title;
+        string author;
+        
+
+    public Book()
+        {
+            bookid = 1;
+            title = "dotnet";
+            author = "mayuri";
+
+        }
+    public Book(Book b2)
+        {
+            bookid = b2.bookid;
+            title = b2.title;
+            author = b2.author;
+        }
+        public void display()
+        {
+            Console.WriteLine("book id : " + bookid);
+            Console.WriteLine("title : " + title);
+            Console.WriteLine("author : " + author);
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            Book book1 = new Book();
+            Book book2 = new Book(book1);
+            Console.WriteLine("book 1 details are : ");
+            book1.display();
+            Console.WriteLine("book 2 details are : ");
+            book2.display();
+            Console.ReadKey();
+        }
+    }
+}
