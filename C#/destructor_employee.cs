6f7ba0f0e31cf88fc904f1e3137710f752a4703f
mayurikale2406 <mayurikale1404@gmail.com>
2023-11-07 22:07:30 +0530

    csharp

---
+++ C#/destructor_employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace program
+{
+    class Employee
+    {
+        public Employee()
+        {
+            Console.WriteLine("constructor called");
+        }
+        ~Employee()
+        {
+            Console.WriteLine("destructor called");
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            Employee emp = new Employee();
+            Console.ReadKey();
+            Employee emp2 = new Employee();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/method_overloading_diff_parameter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace program
+{
+    class calculate
+    {
+        public void additon(int n1,int n2 )
+        {
+            int res = n1 + n2;
+            Console.WriteLine("addition of two int : " + res);
+        }
+        public void addition(float n1,float n2)
+        {
+            float res = n1 + n2;
+            Console.WriteLine("additon of two float : " + res);
+        }
+        class program
+        {
+            static void Main()
+            {
+                calculate cal = new calculate();
+                cal.additon(2, 4);
+                Console.WriteLine("----------------------");
+                Console.WriteLine("addition of two float ");
+                cal.addition(2.2f, 3.5f);
+                Console.ReadKey();
+            }
+        }
+    }
+}
