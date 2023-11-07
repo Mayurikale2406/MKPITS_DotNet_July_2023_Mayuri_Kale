2538e63a315fdabef9cf2ba83ff568a70c20feb5
mayurikale2406 <mayurikale1404@gmail.com>
2023-11-07 22:08:32 +0530

    windowsform

---
+++ Windowsforms/bank_acc_form.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bank_acc_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int balance = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BankCustomer b1 = new BankCustomer();
+            b1.name = textBox1.Text;
+            
+        }
+    }
+}
---
+++ Windowsforms/rectangle_one_form.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rectangle_one_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Rectangle rec = new Rectangle();
+            rec.width = Convert.ToInt32(textBox1.Text);
+            rec.height = Convert.ToInt32(textBox2.Text);
+            rec.area =rec. height +rec.width ;
+            StringBuilder sb = new StringBuilder();
+            sb.Append("area : " + rec.area);
+            label2.Text = sb.ToString();
+           
+            
+        }
+    }
+}
