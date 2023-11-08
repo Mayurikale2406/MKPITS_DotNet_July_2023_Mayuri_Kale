8af27d31acc59de5472339f5b99854faa9d822f7
mayurikale2406 <mayurikale1404@gmail.com>
2023-11-08 23:10:32 +0530

    csharp

---
+++ C#/hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class ConsoleApp1
+{
+    public ConsoleApp1()
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hellow");
+            Console.Readkey();
+  }
+    }
+}
