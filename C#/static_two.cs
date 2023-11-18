6e48f973a2ae386c36e763cf9921bc932027be91
mayurikale2406 <mayurikale1404@gmail.com>
2023-11-18 22:04:31 +0530

    csharp

---
+++ C#/static_two.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class student
+    {
+        public static void display()
+        {
+            Console.WriteLine("hello");
+        }
+        public void sayhi()
+        {
+            Console.WriteLine("hi");
+        }
+    }
+    class Program
+    {
+        //creating nonstatic display method
+
+        static void Main(string[] args)
+        {
+            student.display();
+            //    student.sayhi();// error
+            student s = new student();
+            s.sayhi();
+
+        }
+    }
+}
