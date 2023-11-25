fc15d5bce5648a9df89b540362d03f7626b25b88
mayurikale2406 <mayurikale1404@gmail.com>
2023-11-25 22:49:54 +0530

    windowsform

---
+++ Windowsforms/queue_class_student_form.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace queue_class_student_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Queue q = new Queue();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            q.Enqueue(textBox1.Text);
+            q.Enqueue(textBox2.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(string val in q)
+            {
+                sb.Append(val + "\n");
+               
+            }
+            label3.Text = sb.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            q.Dequeue();
+            foreach(string s in q)
+            {
+                sb.Append(s + "\n");
+               
+            }
+            label4.Text = sb.ToString();
+        }
+    }
+}
