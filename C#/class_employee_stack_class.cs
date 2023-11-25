285a4b9095aa74ad874303609673f41f18e6dcfc
mayurikale2406 <mayurikale1404@gmail.com>
2023-11-25 22:47:09 +0530

    csharp

---
+++ C#/class_employee_stack_class.cs
@@ -0,0 +1,73 @@
+﻿//create a stack  collection of employee class(empno, empname , salary and
+//designation)
+using System;
+using System.Collections;
+namespace program
+{
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public int salary;
+        public string designation;
+
+        public Employee(int empno,string empname,int salary,string designation)
+        {
+            this.empno = empno;
+            this.empname = empname;
+            this.salary = salary;
+            this.designation = designation;
+
+        }
+        class program
+        {
+            static void Main()
+            {
+                Employee e1 = new Employee(11, "mayuri", 50000, "Owener");
+                Employee e2 = new Employee(12, "vrushali", 40000, "manager");
+                Employee e3 = new Employee(13, "sayali", 30000, "developer");
+                Employee e4 = new Employee(14, "vibha", 20000, "coder");
+
+                Stack st = new Stack();
+                Console.WriteLine("After Peek");
+                st.Push(e1);
+                st.Push(e2);
+                st.Push(e3);
+                st.Push(e4);
+
+                foreach(Employee e in st)
+                {
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("Employee Id : " + e.empno);
+                    Console.WriteLine("Employee Name : " + e.empname);
+                    Console.WriteLine("Employee Salary : " + e.salary);
+                    Console.WriteLine("Employee designation : " + e.designation);
+                    Console.WriteLine("-----------------------------");
+               
+
+                }
+                Console.WriteLine("After Pop");
+                st.Pop();
+                
+                    foreach(Employee e in st)
+                {
+                    Console.WriteLine("-----------------------------");
+                    Console.WriteLine("Employee Id : " + e.empno);
+                    Console.WriteLine("Employee Name : " + e.empname);
+                    Console.WriteLine("Employee Salary : " + e.salary);
+                    Console.WriteLine("Employee designation : " + e.designation);
+                    Console.WriteLine("--------------------------------");
+
+                }
+              
+
+
+
+
+
+            }
+           
+        }
+
+    }
+}
---
+++ C#/queue_class_student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+
+namespace program
+{
+    class student
+    {
+        public int rno;
+        public string name;
+        public student (int rno,string name)
+        {
+            this.rno = rno;
+            this.name = name;
+        }
+    }
+    class program
+    {
+        static void Main()
+        {
+            student s1 = new student(11,"amit");
+            student s2 = new student(12, "ravi");
+            student s3 = new student(13, "Manish");
+
+            Queue s = new Queue();
+            s.Enqueue(s1);
+            s.Enqueue(s2);
+            s.Enqueue(s3);
+
+            foreach (student val in s)
+            {
+                Console.WriteLine("rno : " + val.rno);
+                Console.WriteLine("name : " + val.name);
+            }
+            student r = (student) s.Dequeue();
+            Console.WriteLine("------------------------");
+            Console.WriteLine("item dequeue " + r.rno + ": " + r.name);
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("after dequeue");
+            Console.WriteLine("----------------------");
+
+            foreach(student val in s)
+            {
+                Console.WriteLine("name : " + val.name);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
