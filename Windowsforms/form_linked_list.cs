3cd51e5de2168e1bdd65630b13605db0944329a8
mayurikale2406 <mayurikale1404@gmail.com>
2023-11-27 22:11:08 +0530

    windowsform

---
+++ Windowsforms/form_linked_list.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace form_linked_list
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        LinkedList<int> l = new LinkedList<int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            l.AddLast(Convert.ToInt32(textBox1.Text));
+            textBox1.Clear();
+            textBox1.Focus();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            l.AddFirst(Convert.ToInt32(textBox1.Text));
+            textBox1.Clear();
+            textBox1.Focus();
+           
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+           
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (int i in l)
+                {
+                    sb.Append("num : " + i + "\n");
+                }
+                label3.Text = sb.ToString();
+            }
+
+        }
+    }
+}
---
+++ Windowsforms/form_list_form.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace form_list_class
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> l = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            l.Add(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(string s in l)
+            {
+                sb.Append(s + "\n");
+            }
+            label2.Text = sb.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            l.Remove(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            l.Insert(2,textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            l.Sort();
+        }
+    }
+}
