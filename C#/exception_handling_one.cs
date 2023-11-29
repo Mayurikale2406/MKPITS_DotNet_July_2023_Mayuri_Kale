ee3bc0098454c1bf8958d7da93ee433fee3763af
mayurikale2406 <mayurikale1404@gmail.com>
2023-11-29 22:27:03 +0530

    csharp

---
+++ C#/exception_handling_one.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace program
+{
+    class program
+    {
+        static void Main()
+        {
+            int num = 10;
+            int n = 0;
+            int res = num / n;
+            Console.WriteLine("res :" + res);//error will come nd program terminated
+            Console.WriteLine("bye");
+        }
+    }
+}
