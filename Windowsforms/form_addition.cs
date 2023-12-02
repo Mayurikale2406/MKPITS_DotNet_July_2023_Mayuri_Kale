57618fdb6a1cd6738951bcff5876c98153049518
mayurikale2406 <mayurikale1404@gmail.com>
2023-12-02 22:41:18 +0530

    windowsform

---
+++ Windowsforms/form_addition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_addition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox2.Text);
+            int add = num1 + num2;
+            int sub = num1 - num2;
+            int mul = num1 * num2;
+            label3.Text = "Addition of two number : " + add;
+            label4.Text = "Substraction of two number : " + sub;
+            label5.Text = "Multiplication of two number : " + mul;
+
+           
+        }
+    }
+}
---
+++ Windowsforms/vote_form.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vote__form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int age = Convert.ToInt32(textBox1.Text);
+            if(age>=18)
+            {
+                label2.Text = "eligible for vote";
+            }
+            else
+            {
+                label2.Text = "not eligible for vote";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
