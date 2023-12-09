cf8a5bbe4c87c46241016c93392a8000aa529e23
mayurikale2406 <mayurikale1404@gmail.com>
2023-12-09 22:31:51 +0530

    windowsform

---
+++ Windowsforms/hscrollbar_form.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hscrollbar_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void hScrollBar1_Scroll_1(object sender, ScrollEventArgs e)
+        {
+            this.Width = hScrollBar1.Value;
+        }
+
+        private void vScrollBar1_Scroll_1(object sender, ScrollEventArgs e)
+        {
+            this.Height = vScrollBar1.Value;
+        }
+    }
+}
