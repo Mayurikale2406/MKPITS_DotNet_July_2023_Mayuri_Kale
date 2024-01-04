719be1d992d2ee21f301cbc5e359aa6e81f6fb51
mayurikale2406 <mayurikale1404@gmail.com>
2024-01-04 17:24:17 +0530

    sql

---
+++ SQL/customer_form.sql
@@ -0,0 +1,15 @@
+create table customer(custid int primary key,custname varchar(20),
+                      productname varchar(20),productrate int);
+insert into customer values(11,'mayuri','facewash',20)
+insert into customer values(12,'vrushali','eyeshadow',200)
+insert into customer values(13,'sayali','lipstick',2000)
+insert into customer values(14,'mrunal','laptop',30000)
+insert into customer values(15,'priya','shampoo',250)
+insert into customer values(16,'chetan','shirt',400)
+insert into customer values(17,'sahil','chocolate',100)
+insert into customer values(18,'akash','mobile',35000)
+insert into customer values(19,'prachi','oven',32000)
+insert into customer values(10,'vibha','biscuit',50)
+insert into customer values(21,'ayushi','pen',20)
+select * from customer
+
---
+++ SQL/user_form.sql
@@ -0,0 +1,5 @@
+create table users(username varchar(20),
+                   upassword varchar(20));
+insert into users values('mayuri','mayuri1')
+insert into users values('vrushali','vrushali')
+select * from users
