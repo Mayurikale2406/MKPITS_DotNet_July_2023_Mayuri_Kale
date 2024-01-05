8368d690ca336bec942e104d9c85e23db97c09fb
mayurikale2406 <mayurikale1404@gmail.com>
2024-01-05 22:36:23 +0530

    ado.net

---
+++ ADO.NET/ado.form.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace ado.form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String str = "server=.\\SQLEXPRESS01; integrated security=true;database=test1db";
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection(str);
+                con.Open();
+                MessageBox.Show("connection succesfull");
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = Databaseconnection.GetConnection();
+            if(con != null)
+            {
+                MessageBox.Show("succesfull");
+                con.Close();
+            }
+            else
+            {
+                MessageBox.Show("not connect");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
---
+++ ADO.NET/datagridview_form.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datagridview_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("server=.\\SQLEXPRESS01;integrated security = true;database=updb");
+        DataSet ds = new DataSet();
+        SqlDataAdapter da;
+        SqlCommandBuilder cb = null;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            da = new SqlDataAdapter("select * from customer", con);
+            cb = new SqlCommandBuilder(da);
+            da.Fill(ds, "cust");
+            dataGridView1.DataSource = ds.Tables["cust"];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                da.Update(ds.Tables["cust"]);
+                MessageBox.Show("record updated");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+ }
