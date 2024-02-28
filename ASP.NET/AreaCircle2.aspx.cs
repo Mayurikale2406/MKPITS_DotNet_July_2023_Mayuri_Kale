bbb6e5dd782f4bd331f9d4c1bc827b66d104a147
mayurikale2406 <mayurikale1404@gmail.com>
2024-02-28 17:56:06 +0530

    web

---
+++ ASP.NET/AreaCircle2.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace area_circle
+{
+    public partial class AreaCircle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            float area = Convert.ToSingle(TextBox1.Text);
+            
+            area = 3.14f * area * area;
+            Label1.Text = "area of circle : " + area;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            float area = Convert.ToSingle(TextBox1.Text);
+            area = 2 * 3.14f * area;
+            Label2.Text = "area of circ : " + area;
+        }
+    }
+}
